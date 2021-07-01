d9da82dc059d64c4791e2cdbead03e826ef3f2b6
Danilo Gomes <51554360+dantls@users.noreply.github.com>
2021-06-30 23:40:13 -0400

    Deleted Dashboard

--- src/pages/Service/index.js
+++ src/pages/Service/index.js
@@ -1,11 +1,8 @@
-// import {useHistory} from 'react-router';
 import { TableService } from '../../components/TableService';
 import {Container} from './styles';
 
 
 export function Service(){
-    // const history = useHistory();
-
     return (  
       <Container>
         <TableService />
